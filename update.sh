e5ad0b8da5ed6d256d004a15afb2a0d7692d617e
mfmatos <46325500+mfmatos@users.noreply.github.com>
2020-07-24 13:59:42 -0300

    network to create

--- update.sh
+++ update.sh
@@ -1,2 +1,5 @@
+# create network on first run
+# docker network create --driver=overlay web
+
 docker service rm portainer_agent
 docker stack deploy --compose-file=portainer-agent-stack.yml portainer
