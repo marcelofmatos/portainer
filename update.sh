33a1176d344a2f293dcd7ea06d88cea6f49d781e
mfmatos <46325500+mfmatos@users.noreply.github.com>
2022-04-02 12:04:10 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 # create network on first run
 # docker network create --driver=overlay web
 
+. .env
 docker service rm portainer_agent
 docker stack deploy --compose-file=portainer-stack.yml portainer
