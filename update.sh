ed891a1ce417a45d4e9d23f681cacbd756547fc7
Marcelo Matos <marcelofmatos@gmail.com>
2024-04-08 13:28:58 -0400

    feat: portainer stack

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 # create network on first run
 # docker network create --driver=overlay web
 
+cd $( dirname $0 )
+[[ -f .env ]] && source .env
 docker service rm portainer_agent
-docker stack deploy --compose-file=portainer-agent-stack.yml portainer
+docker stack deploy --compose-file=portainer-stack.yml portainer
