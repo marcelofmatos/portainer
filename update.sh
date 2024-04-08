bbc26e99815d4bfb495e1b1dfce357363e184303
Marcelo Matos <marcelofmatos@gmail.com>
2024-04-08 14:03:16 -0400

    feat: portainer and balancer

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 # create network on first run
 # docker network create --driver=overlay web
 
+export PORTAINER_FQDN=${PORTAINER_FQDN:-localhost.localdomain}
+export PROXY_NET=${PROXY_NET:-proxy}
+
 cd $( dirname $0 )
 [[ -f .env ]] && source .env
 docker service rm portainer_agent
