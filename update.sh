a60ed12f5e2e1464a526b888e977522649c97e49
mfmatos <46325500+mfmatos@users.noreply.github.com>
2024-09-02 17:56:17 -0300

    fix: update

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 # docker network create --driver=overlay web
 
 cd $( dirname $0 )
+
+git pull
+
 [[ -f .env ]] && export $(grep -v '^#' .env | xargs)
 docker service rm portainer_agent
 docker stack deploy --compose-file=portainer-stack.yml portainer
